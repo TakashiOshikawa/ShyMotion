b2f22b64e82c8690d679108fe7ac2ebba3c54903
Takashi Oshikawa <abr3tks5200@ezweb.ne.jp>
2015-10-18 04:34:03 +0900

    add function get tweet by user id

---
+++ src/main/scala/tokyo/shymotion/model/UserModel.scala
@@ -0,0 +1,10 @@
+package tokyo.shymotion.model
+
+import tokyo.shymotion.model.table.UserTable
+
+/**
+ * Created by oshikawatakashi on 2015/10/18.
+ */
+object UserModel extends UserTable {
+
+}
--- src/main/scala/tokyo/shymotion/model/table/UserTable.scala
+++ src/main/scala/tokyo/shymotion/model/table/UserTable.scala
@@ -1,10 +1,18 @@
 package tokyo.shymotion.model.table
 
+import play.api.libs.json.Json
 import scalikejdbc.WrappedResultSet
 
 /**
  * Created by oshikawatakashi on 2015/10/12.
  */
+
+trait UserTable {
+
+  implicit def jsonWrites = Json.writes[User]
+  implicit def jsonReads = Json.reads[User]
+
+}
 
 case class User
     (
